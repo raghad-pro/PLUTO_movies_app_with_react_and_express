e0daf506490b478394d5051857ab6e9ba5f8408a
mohammedabusamra04 <mohammedabosamra22@gmail.com>
2026-04-12 21:56:31 +0300

    restore server.js to previous working version

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const app = express();
-const fs = require("fs");
-const path = require("path");
 
 app.use(express.json());
 
@@ -9,28 +7,6 @@ app.get("/test", async (req, res) => {
     res.send("Server is working");
 });
 
-
-const moviesPath = path.join(__dirname, "data", "movies-db.json");
-
-app.get("/movies", (req, res) => {
-    try {
-    const data = fs.readFileSync(moviesPath, "utf-8");
-    const movies = JSON.parse(data);
-    
-    res.status(200).json({
-      success: true,
-      message: "Movies returned correctly",
-      data: movies
-    });
-
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: "Failed to read movies file",
-      error: error.message
-    });
-  }
-});
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
