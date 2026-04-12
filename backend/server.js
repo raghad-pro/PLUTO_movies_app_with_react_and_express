5150f381a09393f26210a67f5e95551a58f95f32
mohammedabusamra04 <mohammedabosamra22@gmail.com>
2026-04-12 22:57:25 +0300

    Add GET /movies/:id endpoint

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const fs = require("fs");
 const path = require("path");
+const { json } = require("stream/consumers");
 
 app.use(express.json());
 
@@ -31,6 +32,33 @@ app.get("/movies", (req, res) => {
     });
   }
 });
+
+app.get("/movies/:id",(req,res)=>{
+  try{
+  const id = req.params.id;
+  const data = fs.readFileSync(moviesPath, "utf-8");
+  const movies = JSON.parse(data);
+  const movie = movies.find((m)=> m.id == id);
+  if(!movie){
+    return res.status(404).json({
+      success: false,
+      message: "Movie not found",
+    });
+  }
+  return res.status(200).json({
+      success: true,
+      message: "Movie found",
+      data: movie,
+  });
+}catch(error){
+  return res.status(500).json({
+    success : false,
+    message: "Something went wrong",
+    error: error.message,
+    });
+  }
+})
+
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
