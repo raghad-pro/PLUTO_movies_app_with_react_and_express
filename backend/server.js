0fead645a8cf8407be014aa3c894864bc5ed76c9
mohammedabusamra04 <mohammedabosamra22@gmail.com>
2026-04-12 23:45:15 +0300

    Add POST /movies endpoint

--- backend/server.js
+++ backend/server.js
@@ -59,6 +59,56 @@ app.get("/movies/:id",(req,res)=>{
   }
 })
 
+app.post("/movies",(req,res)=>{
+  try{
+    const { title, description, year } = req.body;
+    const data = fs.readFileSync(moviesPath, "utf-8");
+    const movies = JSON.parse(data);
+
+    if( !title || !description || !year){
+      return res.status(400).json({
+      success : false,
+      message : "All fields are required",
+      });
+    }
+
+    if(typeof year !== "number"){
+      return res.status(400).json({
+        success : false,
+        message : "Year must be a number",
+      });
+    }
+    const exists = movies.find(m => m.title === title);
+    if(exists){
+      return res.status(409).json({
+      success: false,
+      message: "Movie already exists",
+      });
+    }
+
+    const newMovie = {
+      id : Date.now(),
+      title,
+      description,
+      year
+    };
+    movies.push(newMovie);
+
+    fs.writeFileSync("./data/movies-db.json", JSON.stringify(movies, null, 2));
+
+    return res.status(201).json({
+      success: true,
+      message: "Movie created successfully",
+      data: newMovie,
+    });
+  }catch(error){
+    return res.status(500).json({
+      success: false,
+      message: "Something went wrong",
+      error: error.message,
+    });
+  }
+})
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
